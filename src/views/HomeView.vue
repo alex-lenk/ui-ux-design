1ec5f63e06db5a60df6cff5e684c493298e65f96
AlexLenk <alexlenk.ru@gmail.com>
2022-06-05 13:07:09 +0300

    Created public html folder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <main class="main container">
-    <div class="main__name">СИДОРОВ СЕМЁН</div>
+    <div class="main__content">
+      <div class="main__name">СИДОРОВ СЕМЁН</div>
 
-    <h1 class="main__title">
-      Дизайнер интерфейсов<br>
-      с опытом более 5 лет
-    </h1>
+      <h1 class="main__title">
+        Дизайнер интерфейсов<br>
+        с опытом более 5 лет
+      </h1>
 
-    <p class="main__subtitle">Проектирую интерфейсы с учетом интересов пользователя и задач бизнеса.</p>
+      <p class="main__subtitle">Проектирую интерфейсы с учетом интересов пользователя и задач бизнеса.</p>
 
-    <a href="/portfolio" class="main__btn btn__primary">Портфолио</a>
+      <a href="/portfolio" class="main__btn btn__primary">Портфолио</a>
+    </div>
   </main>
 </template>
